2c97d08f4a7a434560b3a477fb9d30e4a05f8027
Tourvas, Kostas (ATH-MRM) <Kostas.Tourvas@mrm-mccann.gr>
2019-03-28 16:45:34 +0200

    init config values

--- src/Participation.php
+++ src/Participation.php
@@ -5,4 +5,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participation extends Model {
 
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('under-the-cap.participation_table');
+        $this->fillable = config('under-the-cap.participation_fields');
+        parent::__construct($attributes);
+    }
+
 }
